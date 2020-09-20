3f758f33a76064aa6352c1384a4afdbe4d6a90c6
Mironov Vladimir <zidiks228@gmail.com>
2020-09-20 23:10:38 +0300

    feat: add pages

--- vsmuplus/src/app/app-routing.module.ts
+++ vsmuplus/src/app/app-routing.module.ts
@@ -5,6 +5,18 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'post',
+    loadChildren: () => import('./post/post.module').then( m => m.PostPageModule)
+  },
+  {
+    path: 'video',
+    loadChildren: () => import('./video/video.module').then( m => m.VideoPageModule)
+  },
+  {
+    path: 'event',
+    loadChildren: () => import('./event/event.module').then( m => m.EventPageModule)
   }
 ];
 @NgModule({
