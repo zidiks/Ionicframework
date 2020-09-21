560d9ef2a7eaae2645be88f182ceed624b400f54
Mironov Vladimir <zidiks228@gmail.com>
2020-09-21 14:57:23 +0300

    upd: add pwa

--- myApp/src/main.ts
+++ myApp/src/main.ts
@@ -1,8 +1,11 @@
+import { Capacitor } from '@capacitor/core';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +13,5 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+defineCustomElements(window);
